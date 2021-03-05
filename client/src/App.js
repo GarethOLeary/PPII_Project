5fc94bbe5c9db028d2ecb952a083c0522e969f31
Gareth O'Leary <G00375321@gmit.ie>
2021-03-05 13:55:13 +0000

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,26 @@ import './App.css';
 import { Home } from './components/home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Register from "./components/Register";
+import Login from "./components/Login";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render(){
   return (
+    <Provider store={store}>
     <Router>
     <div className="App">
       
         <Switch>
           <Route path='/' component={Home} exact/>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
         </Switch>
     </div>
     </Router>
+    </Provider>
   );
 }
 }
