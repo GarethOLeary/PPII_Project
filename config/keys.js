66fb13824416697e47fb2e0bd03bfb5f6617b15e
Gareth O'Leary <G00375321@gmit.ie>
2021-03-05 11:52:04 +0000

    register and login set up using passport + jsonwebtoken

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin:drq12349@cluster0.hd8xq.mongodb.net/ticketing?retryWrites=true&w=majority" 
+  };
+
+  module.exports = {
+    mongoURI: "mongodb+srv://admin:drq12349@cluster0.hd8xq.mongodb.net/ticketing?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
