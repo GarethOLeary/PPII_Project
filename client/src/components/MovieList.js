feaa53605e1bc55c58e06f7f589e1fc5ebf90db3
Krystian <kopryszek@gmail.com>
2021-03-08 00:31:51 +0000

    Create Modal and Animations for movies page

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
-import Fade from 'react-reveal/Fade';
+import Modal from 'react-modal';
+// importing fade efect animation 
+import Fade from "react-reveal/Fade";
+// importing zoom efect animation 
+import Zoom from "react-reveal/Zoom";
 
 export default class MovieList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movie: null
+        }
+    }
+
+    // implementing openModal which accepts movie as a parameter
+    // when click on movie it should be set as a movie of the state component 
+    openModal = (movie) => {
+        this.setState({ movie });
+    };
+
+    // implementing openModal - movie is set to null
+    closeModal = () => {
+        this.setState({ movie: null });
+    };
+
     render() {
+        // introducting modal inside the render function, using contracting assignment  
+        const {movie} = this.state;
+
         return (
             <div>
                 <Fade bottom cascade>
-                <ul className="movies">
-                    {this.props.moviesList.map((movie) => (
-                        <li key={movie._id}>
-                            <div className="movie">
-                                <a href={"#" + movie._id}>
-                                    <img src={movie.image} alt={movie.title}></img>
+                    <ul className="movies">
+                        {this.props.moviesList.map((movie) => (
+                            <li key={movie._id}>
+                                <div className="movie">
+                                    <a href={"#" + movie._id} onClick={() => this.openModal(movie)}>
+                                        <img src={movie.image} alt={movie.title}></img>
                                         <p>
                                             {movie.title}
                                         </p>
-                                </a>
+                                    </a>
                                     <div className="movie-category">
                                         {movie.category}
                                     </div>
@@ -23,11 +48,39 @@ export default class MovieList extends Component {
                                         {movie.description}
                                     </div>
                                     <button className="button primary">Book</button>
-                             </div>
-                        </li>
-                    ))}
-                        </ul>
-                        </Fade>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </Fade>
+                {
+                    // Using conditional rendering
+                    // isOpen - when there is a movie the modal will be shown 
+                    movie && (<Modal isOpen={true} onRequestClose={this.closeModal}>
+                        <Zoom>
+                            <button className="close-modal" onClick={this.closeModal}>x</button>
+                            <div>
+                                <div className="movie-details-description">
+                                   <img src={movie.image} alt={movie.title}></img>
+                                   <p>
+                                       <strong><h1>{movie.title}</h1></strong>
+                                    </p> 
+                                    <p>
+                                        <strong>Year:</strong> {movie.year}
+                                    </p>
+                                    <p>
+                                        <strong>Category:</strong> {movie.category}
+                                    </p>
+                                    <p>
+                                        <strong>Movie description:</strong> {movie.description}
+                                    </p>
+                                </div>
+                            </div>
+                            <button className="button primary">Book</button>
+                        </Zoom>
+                        </Modal>
+                    )
+                }
             </div>
         )
     }
