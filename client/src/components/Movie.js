0fe49b54d2311481b10053b14142b6e09c13e778
Gareth O'Leary <G00375321@gmit.ie>
2021-04-15 17:49:46 +0100

    Searchbar functionality added where movies show up as you type

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -2,20 +2,19 @@ import React from "react";
 import './movie.css';
 const IMG_API = "http://image.tmdb.org/t/p/w1280"
 
-const Movie = ({title, poster_path, overview,
- vote_average}) => (
-  
- <div className="movie">
-  <img src= {IMG_API + poster_path} alt={title} />
-  <div className="movie-info">
-    <h3>{title}</h3>
-    <span>{vote_average}</span>
-  
-  </div>
-  <div className="movie-over">
-    <h2>Overview: </h2>
-    <p>{overview}</p>
-  </div>
-</div>
- );
+const Movie = ({ title, poster_path, overview, movieId,
+  vote_average }) => (
+
+    <div className="movie">
+      <a href={`/movie/${movieId}`} >
+        <img src={IMG_API + poster_path} alt={title} />
+      </a>
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span>{vote_average}</span>
+
+      </div>
+
+    </div>
+  );
 export default Movie;
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -6,21 +6,11 @@ import { NavLink, Link } from 'react-router-dom'
 import { get } from "mongoose";
 import LoadMoreBtn from './LoadMoreBtn'
 import Spinner from './Spinner'
+import Search from "./Search"
 
-
-
-
-const API_URL2 = 'https://api.themoviedb.org/3/';
+const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '12a3069539c26ded272cb55534169534';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=12a3069539c26ded272cb55534169534&query=';
-
-// For categoty eg horror, comedian etc 
-// https://api.themoviedb.org/3/discover/movie?sort_by=comedian&api_key=12a3069539c26ded272cb55534169534
-
-// Images
-// An image URL looks like this example:
-// http://image.tmdb.org/t/p/w780/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg
-
 
 class Home1 extends React.Component {
     state = {
@@ -33,26 +23,44 @@ class Home1 extends React.Component {
 
     // Life cycle method 
     componentDidMount() {
-        if(localStorage.getItem('Homestate')){
+        if (localStorage.getItem('Homestate')) {
             const state = JSON.parse(localStorage.getItem('Homestate'));
-            this.setState({...state});
+            this.setState({ ...state });
         } else {
-            this.setState({loading: true });
-            const endpoint = `${API_URL2}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+            this.setState({ loading: true });
+            const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
             this.fetchItems(endpoint);
         }
     }
 
-    
+    searchItems = (searchTerm) => {
+        console.log(searchTerm);
+
+        let endpoint = '';
+        this.setState({
+            movies: [],
+            loading: true,
+            searchTerm: searchTerm
+        })
+
+        if (searchTerm === '') {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        } else {
+            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+        }
+
+        this.fetchItems(endpoint);
+    }
+
 
     loadMoreItems = () => {
         let endpoint = '';
-        this.setState({loading: true});
+        this.setState({ loading: true });
 
-        if(this.state.searchTerm === ''){
-            endpoint = `${API_URL2}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+        if (this.state.searchTerm === '') {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
         } else {
-            endpoint = `${API_URL2}search/movie?api_key=${API_KEY}&language=en-US&query=${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
         }
 
         this.fetchItems(endpoint);
@@ -60,52 +68,52 @@ class Home1 extends React.Component {
 
     fetchItems = (endpoint) => {
         fetch(endpoint)
-        .then(result => result.json())
-        .then(result => {
-           this.setState({
-               // Spread operator to append to current array
-               movies: [...this.state.movies,...result.results],
-               // If null, it will fetch result
-               loading: false,
-               currentPage: result.page,
-               totalPages: result.total_pages
-           }, () => {
-               if(this.state.searchTerm === "") {
-                localStorage.setItem('HomeState', JSON.stringify(this.state));
-               }
-           })
-        });    
+            .then(result => result.json())
+            .then(result => {
+                this.setState({
+                    // Spread operator to append to current array
+                    movies: [...this.state.movies, ...result.results],
+                    // If null, it will fetch result
+                    loading: false,
+                    currentPage: result.page,
+                    totalPages: result.total_pages
+                }, () => {
+                    if (this.state.searchTerm === "") {
+                        localStorage.setItem('HomeState', JSON.stringify(this.state));
+                    }
+                })
+            });
     }
 
     render() {
         return (
-            
+
             <div className="rmdb-home">
-    
-            <div>
-                
-                
-            </div>
-             <div className="rmdb-home-grid">
-                <div 
-                    header={this.state.searchTerm ? 'Search result' : 'Popular Movies'} 
-                    loading={this.state.loading} >
-                    {this.state.movies.map((element, i) => {
-                        return <Movie 
-                                    key={i} 
-                                    clickable={true} 
-                                    poster_path={element.poster_path}
+
+                <div>
+                    <Search callback={this.searchItems} />
+
+                </div>
+                <div className="rmdb-home-grid">
+                    <div
+                        header={this.state.searchTerm ? 'Search result' : 'Popular Movies'}
+                        loading={this.state.loading} >
+                        {this.state.movies.map((element, i) => {
+                            return <Movie
+                                key={i}
+                                clickable={true}
+                                poster_path={element.poster_path}
                                 movieId={element.id}
                                 title={element.original_title}
                                 vote_average={element.vote_average}
-                                />
-                    })}
+                            />
+                        })}
+                    </div>
+                    {this.state.loading ? <Spinner /> : null}
+                    {(this.state.currentPage <= this.state.totalPages && !this.state.loading) ?
+                        <LoadMoreBtn text="Load More" onClick={this.loadMoreItems} /> : null
+                    }
                 </div>
-                {this.state.loading ? <Spinner/> : null}
-                {(this.state.currentPage <= this.state.totalPages && !this.state.loading) ? 
-                  <LoadMoreBtn text="Load More" onClick={this.loadMoreItems}/> : null
-                }
-             </div>   
 
             </div>
         )
